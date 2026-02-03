aa037ad947f8ff23efb3b992b251172795ad8de3
Satyam Rathod <satyamrathod57@gmail.com>
2026-02-03 15:10:18 +0530

    created code for problem

---
+++ main.py
@@ -0,0 +1,33 @@
+# Number of monsters
+n = int(input())
+
+# Your starting power
+current_power = int(input())
+
+# Required power for each monster
+power = []
+for _ in range(n):
+    power.append(int(input()))
+
+# Bonus power gained after defeating each monster
+bonus = []
+for _ in range(n):
+    bonus.append(int(input()))
+
+# Combine power and bonus into pairs
+monsters = list(zip(power, bonus))
+
+# Sort monsters by required power (smallest first)
+monsters.sort()
+
+defeated = 0  # count how many monsters we defeat
+
+for req_power, gain in monsters:
+    if current_power >= req_power:
+        current_power += gain  # gain bonus power
+        defeated += 1
+    else:
+        break  # can't defeat further monsters
+
+print(defeated)
+
